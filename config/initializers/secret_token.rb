f644ca60a026f29c618b4a96daf3456de55f031f
Dave Guarino <dave@codeforamerica.org>
2013-06-04 10:35:22 -0700

    Initial with RSpec installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Automidnight::Application.config.secret_key_base = '4b1e071e0c6f4774088668dd143b1192d762a23d7b14d27f90e8297d48b8dde273d8172454ad9ce91db20b7fcb3e544be2c3cdc28443d5a3f64f31f31b649a95'
---
+++ spec/test_rspec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe "rspec tests exist" do
+  it "passes" do
+    true.should be_true
+  end
+end
