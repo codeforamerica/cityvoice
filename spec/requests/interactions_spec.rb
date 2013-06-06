596a82e33f225792b6e469d0dd72868dd3d7ffd6
Dave Guarino <dave@codeforamerica.org>
2013-06-05 17:48:35 -0700

    Remove redundant test

--- spec/requests/interactions_spec.rb
+++ spec/requests/interactions_spec.rb
@@ -12,11 +12,6 @@ describe "V&A SMS App" do
     twilio_body_from(response).should eq("Thanks! You have selected A - Demolish for property 3456. You can see responses and learn more at www.FeedbackApp.com.")
   end
 
-  it "responds to a number + letter with confirmation" do
-    post '/vacant', Body: "3456 A"
-    twilio_body_from(response).should eq("Thanks! You have selected A - Demolish for property 3456. You can see responses and learn more at www.FeedbackApp.com.")
-  end
-
   it "correctly deals with number-then-letter input" do
     pending "Implementation of number-then-letter logic"
     post '/vacant', Body: "3465"
