f07fb1eb6ce4f4e533a2b6b544d21e2c2bb129ed
Dave Guarino <dave@codeforamerica.org>
2013-06-05 17:09:54 -0700

    Change controller method name to handle_feedback

--- app/controllers/text_feedback_controller.rb
+++ app/controllers/text_feedback_controller.rb
@@ -3,7 +3,7 @@ class TextFeedbackController < ApplicationController
   @@code_hash = { "A" => "Demolish", "B" => "Rehab", "C" => "Other" }
   @@app_url = "www.FeedbackApp.com"
 
-  def reply 
+  def handle_feedback 
     #binding.pry
     text_body = params["Body"]
     reply_text = "Thanks! You have selected #{text_body[5]} - #{@@code_hash[text_body[5]]} for property #{text_body[0..3]}. You can see responses and learn more at #{@@app_url}."
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Automidnight::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  post '/vacant' => 'text_feedback#reply'
+  post '/vacant' => 'text_feedback#handle_feedback'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
