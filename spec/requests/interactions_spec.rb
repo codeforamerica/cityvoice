3539672523900151fe9a4323eac4cd85bfc94eb0
Dave Guarino <dave@codeforamerica.org>
2013-06-08 21:55:01 -0700

    Refactor tests with before block

--- spec/requests/interactions_spec.rb
+++ spec/requests/interactions_spec.rb
@@ -7,15 +7,24 @@ describe "V&A SMS App" do
     returned_text = parsed_response_body.xpath('//Response/Sms').text
   end
 
+  before(:all) do
+    @property_number = "1234"
+    @choice_code = "A"
+    @choice_text = "Demolish"
+    @success_message = "Thanks! You have selected #{@choice_code} - #{@choice_text} for property #{@property_number}. You can see responses and learn more at www.FeedbackApp.com."
+  end
+
   it "responds to a number + letter with confirmation" do
-    post 'vacant', :Body => "3456 A"
-    twilio_body_from(response).should eq("Thanks! You have selected A - Demolish for property 3456. You can see responses and learn more at www.FeedbackApp.com.")
+    post 'vacant', :Body => "#{@property_number} #{@choice_code}"
+    twilio_body_from(response).should eq(@success_message)
   end
 
   it "correctly deals with number-then-letter input" do
     pending "Implementation of number-then-letter logic"
-    post '/vacant', Body: "3465"
-    twilio_body_from(response).should eq("You have selected property 3456. To submit a response please text A for Demolish, B for Rehab, C for Other.")
+
+    # Still need to refine language
+    post '/vacant', Body: @property_number
+    twilio_body_from(response).should eq("for Demolish for the property at 1234 56th Ave.  You can see responses and learn more at FeedbackApp.com/#{@prop_num}.")
     post '/vacant', Body: "A"
     twilio_body_from(response).should eq("Thanks! You have selected A - Demolish for property 3456.  You can see responses and learn more at www.FeedbackApp.com.") 
   end
