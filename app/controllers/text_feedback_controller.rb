468f68a90815976e3ad23037f4a8aa1d4fdec076
Dave Guarino <dave@codeforamerica.org>
2013-06-10 10:16:48 -0700

    Added successful-comment message

--- app/controllers/text_feedback_controller.rb
+++ app/controllers/text_feedback_controller.rb
@@ -5,11 +5,14 @@ class TextFeedbackController < ApplicationController
 
   def handle_feedback 
     @feedback = FeedbackSms.new(params)
-    if @feedback.valid?
+    if session[:expect_comment]
+      reply_text = "Thanks for your comment! City staff will review your feedback. Please tell your neighbors about this program."
+    elsif @feedback.valid?
       reply_text = "Thanks! We recorded your response '#{@@code_hash[@feedback.text[4]]}' for #{@feedback.address}. Reply with your comments and visit #{@@app_url}/#{@feedback.text[0..3]} to learn more." 
+      session[:expect_comment] = true
     elsif !session[:failed_once?]
-      session[:failed_once?] = true
       reply_text = "Sorry, we didn't understand your response. Please text back one of the exact choices on the sign, like '1234O' or '1234R'."
+      session[:failed_once?] = true
     else
       reply_text = "We're very sorry, but we still don't understand your response. Please visit 1000in1000.com or call 123-456-7890 to submit your feedback."
     end
--- spec/requests/interactions_spec.rb
+++ spec/requests/interactions_spec.rb
@@ -13,6 +13,7 @@ describe "V&A SMS App" do
     @choice_code = "D"
     @choice_text = "Demolish"
     @success_message = "Thanks! We recorded your response '#{@choice_text}' for #{@property_address}. Reply with your comments and visit 1000in1000.com/#{@property_number} to learn more." 
+    @successful_comment_message = "Thanks for your comment! City staff will review your feedback. Please tell your neighbors about this program."
     @fail1_message = "Sorry, we didn't understand your response. Please text back one of the exact choices on the sign, like '1234O' or '1234R'."
     @fail2_message = "We're very sorry, but we still don't understand your response. Please visit 1000in1000.com or call 123-456-7890 to submit your feedback."
   end
@@ -20,6 +21,13 @@ describe "V&A SMS App" do
   it "responds to good input with success message" do
     post 'vacant', :Body => "#{@property_number+@choice_code}"
     twilio_body_from(response).should eq(@success_message)
+  end
+
+  it "responds to comment with successful comment message" do
+    post 'vacant', :Body => "#{@property_number+@choice_code}"
+    twilio_body_from(response).should eq(@success_message)
+    post 'vacant', :Body => "tear down this wall"
+    twilio_body_from(response).should eq(@successful_comment_message)
   end
 
   it "correctly deals with wrong-then-correct" do
