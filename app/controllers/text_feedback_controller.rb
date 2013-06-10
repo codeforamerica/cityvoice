474d357e86be6ebb28307faa956fb6c17bd05900
Dave Guarino <dave@codeforamerica.org>
2013-06-10 09:36:42 -0700

    Implemented badinput-then-badinput successfull

--- app/controllers/text_feedback_controller.rb
+++ app/controllers/text_feedback_controller.rb
@@ -7,8 +7,11 @@ class TextFeedbackController < ApplicationController
     @feedback = FeedbackSms.new(params)
     if @feedback.valid?
       reply_text = "Thanks! We recorded your response '#{@@code_hash[@feedback.text[4]]}' for #{@feedback.address}. Visit #{@@app_url}/#{@feedback.text[0..3]} to see what other people had to say."
-    else
+    elsif !session[:failed_once?]
+      session[:failed_once?] = true
       reply_text = "Sorry, we didn't understand your response. Please text back one of the exact choices on the sign, like '1234O' or '1234R'."
+    else
+      reply_text = "We're very sorry, but we still don't understand your response. Please visit 1000in1000.com or call 123-456-7890 to submit your feedback."
     end
     render :inline => TextReply.new(reply_text).body
   end
--- spec/requests/interactions_spec.rb
+++ spec/requests/interactions_spec.rb
@@ -14,6 +14,7 @@ describe "V&A SMS App" do
     @choice_text = "Demolish"
     @success_message = "Thanks! We recorded your response '#{@choice_text}' for #{@property_address}. Visit 1000in1000.com/#{@property_number} to see what other people had to say."
     @fail1_message = "Sorry, we didn't understand your response. Please text back one of the exact choices on the sign, like '1234O' or '1234R'."
+    @fail2_message = "We're very sorry, but we still don't understand your response. Please visit 1000in1000.com or call 123-456-7890 to submit your feedback."
   end
 
   it "responds to good input with success message" do
@@ -27,6 +28,13 @@ describe "V&A SMS App" do
     twilio_body_from(response).should eq(@fail1_message)
     post '/vacant', Body: "#{@property_number+@choice_code}"
     twilio_body_from(response).should eq(@success_message)
+  end
+
+  it "correctly deals with wrong-then-wrong" do
+    post '/vacant', Body: "wat"
+    twilio_body_from(response).should eq(@fail1_message)
+    post '/vacant', Body: "WAT"
+    twilio_body_from(response).should eq(@fail2_message)
   end
 
 end
