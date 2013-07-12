30ea1416ce7ad250f79d9c0275588551ef2f5962
Dave Guarino <dave@codeforamerica.org>
2013-07-12 15:21:47 -0700

    Oops, forgot schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130710205421) do
+ActiveRecord::Schema.define(version: 20130712220348) do
 
   create_table "feedback_inputs", force: true do |t|
     t.integer  "question_id"
@@ -41,6 +41,12 @@ ActiveRecord::Schema.define(version: 20130710205421) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "property_code"
+  end
+
+  create_table "voice_files", force: true do |t|
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "voice_transcriptions", force: true do |t|
