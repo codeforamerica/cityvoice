a8a863587bdcad04b96d5107be62e73abebe6756
Dave Guarino <dave@codeforamerica.org>
2013-08-06 23:41:20 -0700

    MarketCluster working with test lat longs

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,1 +1,6 @@
-window.map = L.mapbox.map('map','codeforamerica.map-stwhr1eg').setView([41.687, -86.254], 15);
+window.map = L.mapbox.map('map','codeforamerica.map-stwhr1eg').setView([41.687, -86.254], 12);
+var markers = new L.MarkerClusterGroup();
+markers.addLayer(new L.marker([41.687, -86.254],[41.6871, -86.254],[41.6872, -86.254]));
+markers.addLayer(new L.marker([41.6871, -86.254]));
+markers.addLayer(new L.marker([41.6872, -86.254]));
+map.addLayer(markers);
