fd39d2827dc1475359c31ed5fa17ba8af265f17c
Dave Guarino <dave@codeforamerica.org>
2013-08-23 09:45:24 -0700

    Remove spaces from secret token error message

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,9 +10,7 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 if Rails.env.production? && ENV['SECRET_TOKEN'].blank?
-  raise 'The SECRET_TOKEN environment variable is not set.\n
-    To generate it, run "rake secret", then set it on the production server.\n
-    If you\'re using Heroku, you do this with "heroku config:set SECRET_TOKEN=the_token_you_generated"'
+  raise 'The SECRET_TOKEN environment variable is not set. To generate it, run "rake secret", then set it on the production server. If you\'re using Heroku, you do this with "heroku config:set SECRET_TOKEN=the_token_you_generated"'
 end
 
 Automidnight::Application.config.secret_key_base = ENV['SECRET_TOKEN'] || '4b1e071e0c6f4774088668dd143b1192d762a23d7b14d27f90e8297d48b8dde273d8172454ad9ce91db20b7fcb3e544be2c3cdc28443d5a3f64f31f31b649a95'
