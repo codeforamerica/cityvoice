e92625682137885b3e1b284ce4fe1b61083fbced
Mike Holubowski <mike.holubowski@gmail.com>
2013-09-11 15:49:09 -0700

    Small commit - before working on renaming NotificatoinSubscriber model

--- app/models/feedback_input.rb
+++ app/models/feedback_input.rb
@@ -3,12 +3,18 @@ class FeedbackInput < ActiveRecord::Base
   belongs_to :property
 
   after_create :send_notification_email
+  #  -- or --
+  after_create :add_to_notification_que
 
   def send_notification_email
+    # TODO - Mike
     binding.pry
-    # check if there are any subscribers
-      # self.property.notification_subscribers.where(confirmed: true)
-    # send an email to every subscriber where confirmed:true
+    # find confirmed subscribers to this property:
+    # self.property.notification_subscribers.where(confirmed: true)
+
+    # send email to all of them
   end
+
+
 
 end
