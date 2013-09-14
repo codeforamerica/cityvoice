4d7e73e6fbd1981a297bc55e36a127079ed099df
Mike Holubowski <mike.holubowski@gmail.com>
2013-09-13 17:07:49 -0700

    Light Refactor of Notifier code
    - Broke up main method into 3 seperate ones
    - Should be easier to test
    - Can use any medium for notification (sms, email, in app)
    '

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,5 +1,4 @@
 # Logic for DB roundup queries & email sending
-
 class Notifier
 
   # Construct a hash of the form:
@@ -12,12 +11,23 @@ class Notifier
   # }
   def self.send_weekly_notifications
     puts 'Start of notifier method'
-    # Get the NotificationSubscriptions which have had activity since last email was sent
-    subs = NotificationSubscription.includes(property: :feedback_inputs)
-                                   .where(confirmed: true)
-                                   .where('feedback_inputs.created_at >= notification_subscriptions.last_email_sent_at')
 
-    # Group all the <NotificationSubscription>'s by email
+    subs = self.subscription_with_activity_since_last_email_sent
+
+    emails_hash = self.build_hash_for_mailer(subs)
+
+    self.deliver_emails(emails_hash)
+  end
+
+  # Get the NotificationSubscriptions which have had activity since last notification email was sent
+  def self.subscription_with_activity_since_last_email_sent
+    NotificationSubscription.includes(property: :feedback_inputs)
+                            .where(confirmed: true)
+                            .where('feedback_inputs.created_at >= notification_subscriptions.last_email_sent_at')
+  end
+
+  # Group all the <NotificationSubscription>'s by email
+  def self.build_hash_for_mailer(subs)
     result = Hash.new { |hash, k| hash[k] = {} }
     result.tap do |result|
       subs.each do |sub|
@@ -33,9 +43,11 @@ class Notifier
         sub.update_attributes(last_email_sent_at: DateTime.now)
       end
     end
+  end
 
-    # Deliver for each email address in hash
-    result.each do |email, properties|
+  # Deliver for each email address in hash
+  def self.deliver_emails(emails_hash)
+    emails_hash.each do |email, properties|
       puts "Outbound to #{email}: #{properties.count}"
       NotificationMailer.weekly_activity2(email, properties[:properties]).deliver
     end
