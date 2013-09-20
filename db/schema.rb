617163e4d0f46b64a04cdb4816ccc6c0c1bc11ab
Dave Guarino <dave@codeforamerica.org>
2013-09-20 12:27:17 -0700

    Add description to Subject model and modify view to show it in lieu of property info when PI doesn\'t exist.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130917164747) do
+ActiveRecord::Schema.define(version: 20130920191903) do
 
   create_table "app_content_sets", force: true do |t|
     t.string   "issue"
@@ -66,6 +66,19 @@ ActiveRecord::Schema.define(version: 20130917164747) do
     t.integer  "voice_file_id"
   end
 
+  create_table "rails_admin_histories", force: true do |t|
+    t.text     "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      limit: 2
+    t.integer  "year",       limit: 5
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], name: "index_rails_admin_histories"
+
   create_table "subjects", force: true do |t|
     t.string   "name"
     t.integer  "neighborhood_id"
@@ -76,7 +89,26 @@ ActiveRecord::Schema.define(version: 20130917164747) do
     t.string   "parcel_id"
     t.string   "lat"
     t.string   "long"
+    t.text     "description"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                              default: "", null: false
+    t.string   "encrypted_password",     limit: 128, default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                      default: 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "voice_files", force: true do |t|
     t.string   "url"
