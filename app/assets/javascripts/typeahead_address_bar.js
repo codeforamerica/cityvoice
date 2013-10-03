5d3d0326447416dfbd444fbff9b0682f22791076
Dave Guarino <dave@codeforamerica.org>
2013-10-03 10:26:27 -0700

    Change to root

--- app/assets/javascripts/typeahead_address_bar.js
+++ app/assets/javascripts/typeahead_address_bar.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('#appendedInputButton')
-    .typeahead({name: "Address", prefetch: "subjects.json?only=name" })
+    .typeahead({name: "Address", prefetch: "/subjects.json?only=name" })
     .bind('typeahead:selected', function(obj, datum) {
       window.location = ("/subjects/" + datum.value.replace(/\s/g,"-"));
   });
