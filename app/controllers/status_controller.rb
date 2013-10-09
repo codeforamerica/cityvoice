1584cdf18ba8dd15e5224a86d71d00f392bb2059
Dave Guarino <dave@codeforamerica.org>
2013-10-09 16:32:05 -0700

    Successful testing

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,5 +1,37 @@
 class StatusController < ApplicationController
   def check
-    render :inline => "".to_json
+    response_hash = Hash.new
+    #{ :status => "ok", :updated => "", :dependencies => "", :resources => "" }
+    response_hash[:dependencies] = [ "twilio", "sendgrid", "postgres", "mapbox", "s3", "data.southbendin.gov" ]
+    response_hash[:status] = everything_ok? ? "ok" : "NOT OK"
+    # Dependencies explanation
+    # Twilio - core part of app interaction and storage for voice messages left by users
+    # Postgres - normal Heroku add-on
+    # Mapbox - on CFA account currently
+    #   Look into how to check status on this
+    # S3 - stores voice files played back by voice app
+    # data.southbendin.gov - SB's Socrata portal, from which we pull private data
+    render :inline => response_hash.to_json
+  end
+
+  private
+  def everything_ok?
+    # Check that we have some database presence and core data is available
+    database_okay? && twilio_response_okay?
+  end
+
+  def database_okay?
+    Subject.first.present?
+  end
+
+  def twilio_response_okay?
+    uri = URI('http://www.southbendvoices.com/route_to_survey')
+    res = Net::HTTP.post_form(uri, {})
+    index_of_welcome_message = res.body.index("welcome.mp3")
+    if index_of_welcome_message
+      true
+    else
+      false
+    end
   end
 end
--- spec/controllers/status_controller_spec.rb
+++ spec/controllers/status_controller_spec.rb
@@ -1,9 +1,21 @@
 require 'spec_helper'
 
 describe StatusController do
-  it "returns a json response" do
+  let(:response_hash) { JSON.parse response.body }
+  before do
+    @my_subject = Subject.find_or_create_by(:name => "My little pony")
     get :check
+  end
+  it "returns a json response" do
     response.status.should eq(200)
   end
-
+  it "lists all the dependencies of the app" do
+    response_hash["dependencies"].should eq(%w( twilio sendgrid postgres mapbox s3 data.southbendin.gov ))
+  end
+  it "returns a status of 'ok'" do
+    response_hash["status"].should eq("ok")
+  end
+  after do
+    @my_subject.destroy
+  end
 end
