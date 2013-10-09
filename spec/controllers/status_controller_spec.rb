248bcb732af2cc4e1ab362e5260192e821173bce
Dave Guarino <dave@codeforamerica.org>
2013-10-09 16:49:13 -0700

    GREEN FOR EXTREME

--- spec/controllers/status_controller_spec.rb
+++ spec/controllers/status_controller_spec.rb
@@ -2,20 +2,30 @@ require 'spec_helper'
 
 describe StatusController do
   let(:response_hash) { JSON.parse response.body }
+
   before do
     @my_subject = Subject.find_or_create_by(:name => "My little pony")
     get :check
   end
+
   it "returns a json response" do
     response.status.should eq(200)
   end
+
   it "lists all the dependencies of the app" do
     response_hash["dependencies"].should eq(%w( twilio sendgrid postgres mapbox s3 data.southbendin.gov ))
   end
+
+  it "has a resources key (empty hash for now" do
+    response_hash["resources"].should_not be_nil
+  end
+
   it "returns a status of 'ok'" do
     response_hash["status"].should eq("ok")
   end
+
   after do
     @my_subject.destroy
   end
+
 end
