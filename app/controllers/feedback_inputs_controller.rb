c92f019b31fbbc87c78044de96cc4c4c4abcbf76
Dave Guarino <dave@codeforamerica.org>
2013-10-23 09:46:21 -0700

    Add in parcel ID to CSV export

--- app/controllers/feedback_inputs_controller.rb
+++ app/controllers/feedback_inputs_controller.rb
@@ -12,20 +12,21 @@ class FeedbackInputsController < ApplicationController
     @repair_counts = FeedbackInput.where(:numerical_response => 1).joins(:subject).group(:subject).count(:numerical_response)
     @remove_counts = FeedbackInput.where(:numerical_response => 2).joins(:subject).group(:subject).count(:numerical_response)
     @counts_hash = Hash.new
-    @total_counts.each { |tc| @counts_hash[tc[0].name] = { :total => tc[1] } }
+    @total_counts.each { |tc| @counts_hash[tc[0].name] = { :total => tc[1], :parcel_id => tc[0].parcel_id } }
     @repair_counts.each { |rc| @counts_hash[rc[0].name][:repair] = rc[1] }
     @remove_counts.each { |rc| @counts_hash[rc[0].name][:remove] = rc[1] }
     @counts_array = Array.new
     @counts_hash.each { |elem| @counts_array << elem }
     @sorted_array = @counts_array.sort_by { |elem| elem[1][:total].to_i }.reverse
+    binding.pry
     respond_to do |format|
       format.html { render 'most_feedback' }
       format.csv do
         require 'csv'
         csv_output = CSV.generate do |csv|
-          csv << ["Address", "Total", "Repair", "Remove"]
+          csv << ["Address", "Parcel ID", "Total", "Repair", "Remove"]
           @sorted_array.each do |element|
-            csv << [element[0], element[1].fetch(:total) { 0 }, element[1].fetch(:repair) { 0 }, element[1].fetch(:remove) { 0 }]
+            csv << [element[0], element[1].fetch(:parcel_id) { "N/A" }, element[1].fetch(:total) { 0 }, element[1].fetch(:repair) { 0 }, element[1].fetch(:remove) { 0 }]
           end
         end
         send_data csv_output
