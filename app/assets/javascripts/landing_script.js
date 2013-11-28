2d7b7a9e038237d2866268c32ec3eb39c6e080e9
Dave Guarino <dave@codeforamerica.org>
2013-11-28 09:11:53 -0800

    Change Mapbox to use ENV var-based map (rebased)

--- app/assets/javascripts/landing_script.js
+++ app/assets/javascripts/landing_script.js
@@ -53,10 +53,10 @@ $(document).ready(function() {
 
 function drawMap () {
   if(monroePilot) {
-    window.map = L.mapbox.map('map','codeforamerica.map-stwhr1eg').setView([41.6696, -86.246], 16);
+    window.map = L.mapbox.map('map',mapboxMapID).setView([41.6696, -86.246], 16);
   }
   else {
-    window.map = L.mapbox.map('map','codeforamerica.map-stwhr1eg').setView([41.665, -86.28], 13);
+    window.map = L.mapbox.map('map',mapboxMapID).setView([41.665, -86.28], 13);
   }
   $.getJSON('/subjects.json', placeMarkers);
   $( "#dialog" ).dialog();
