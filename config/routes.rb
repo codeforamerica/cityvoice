1c104d25ec3c1ae474e2f1ce23f139b9d78d1601
Doc Ritezel <pair+doc@ministryofvelocity.com>
2014-03-07 13:17:54 -0800

    Remove commented-out lines and reorder routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,89 +1,21 @@
 Automidnight::Application.routes.draw do
-
-  get "notification_subscriptions/confirm"
-  get "notification_subscriptions/unsubscribe"
-
-  # Comment out resource route for now; eventually use
-  #resources :app_content_sets
-
-  get "/" => "landing#location_search"
-
-  get "/.well-known/status" => "status#check"
-
-  resources :notification_subscriptions, only: [:create]
-
-  #resources :questions
-
-  #resources :feedback_inputs
-
-  resources :subjects, :only => [:index, :show]
-
-  #resources :voice_transcriptions
-
-  #get 'properties/:address' => 'subjects#property_address'
-
+  get 'feedback', to: 'feedback_inputs#most_feedback', as: :most_feedback
   get 'voice-messages', to: 'feedback_inputs#voice_messages', as: :voice_messages
 
-  get 'feedback', to: 'feedback_inputs#most_feedback', as: :most_feedback
+  get '/' => 'landing#location_search'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  resources :notification_subscriptions, only: [:create]
+  get 'notification_subscriptions/confirm'
+  get 'notification_subscriptions/unsubscribe'
 
-  #post '/sms' => 'text_feedback#handle_feedback'
-  #post '/' => 'voice_transcriptions#ask_for_response'
-  #post '/voice' => 'voice_feedback#splash_message'
-  #post '/respond_to_property_code' => 'voice_feedback#respond_to_property_code'
-  #post '/solicit_comment' => 'voice_feedback#solicit_comment'
+  get '/.well-known/status' => 'status#check'
 
-  post '/route_to_survey' => 'voice_feedback#route_to_survey'
-  post '/listen_to_messages_prompt' => 'voice_feedback#listen_to_messages_prompt'
+  resources :subjects, only: [:index, :show]
+
   post '/check_for_messages' => 'voice_feedback#check_for_messages'
-  post '/message_playback' => 'voice_feedback#message_playback'
   post '/consent' => 'voice_feedback#consent'
+  post '/listen_to_messages_prompt' => 'voice_feedback#listen_to_messages_prompt'
+  post '/message_playback' => 'voice_feedback#message_playback'
+  post '/route_to_survey' => 'voice_feedback#route_to_survey'
   post '/voice_survey' => 'voice_feedback#voice_survey'
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
