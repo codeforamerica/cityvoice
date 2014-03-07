72e90232d84a51be34bd28ca0f67876a4b1fa480
Doc Ritezel <pair+doc@ministryofvelocity.com>
2014-03-07 13:32:52 -0800

    Remove dead code

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -3,22 +3,9 @@ module Survey
     if survey_type == "neighborhood"
       ["public_safety", "property_values", "neighborhood_comments"]
     elsif survey_type == "property"
-      # Old, combined
-      #["property_outcome", "public_safety", "property_values", "neighborhood_comments", "property_comments"]
-      # New -- property questions only
       ["property_outcome", "property_comments"]
     elsif survey_type == "iwtw"
       ["i_wish_comment"]
     end
   end
-
-=begin
-  attr_accessor :questions
-  def initialize(survey_type)
-    if survey_type == "neighborhood"
-      @questions = ["public_safety", "property_values", "neighborhood_comments"]
-    end
-  end
-=end
-
 end
