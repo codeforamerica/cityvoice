7c215d1fa41c9c30c2edf4e66bcfab66bb2ce7ff
Doc Ritezel <pair+doc@ministryofvelocity.com>
2014-03-07 13:20:32 -0800

    Remove unused parts of feedback inputs controller

--- app/controllers/feedback_inputs_controller.rb
+++ app/controllers/feedback_inputs_controller.rb
@@ -1,14 +1,6 @@
 class FeedbackInputsController < ApplicationController
-  before_action :set_feedback_input, only: [:show, :edit, :update, :destroy]
-
-  # GET /feedback_inputs
-  # GET /feedback_inputs.json
-  def index
-    @feedback_inputs = FeedbackInput.all
-  end
-
   def most_feedback
-    @total_counts = FeedbackInput.where.not(:numerical_response => nil).joins(:subject).group(:subject).count(:numerical_response)#.map { |item| { item[0].name => { :total => item[1] } } }
+    @total_counts = FeedbackInput.where.not(:numerical_response => nil).joins(:subject).group(:subject).count(:numerical_response)
     @repair_counts = FeedbackInput.where(:numerical_response => 1).joins(:subject).group(:subject).count(:numerical_response)
     @remove_counts = FeedbackInput.where(:numerical_response => 2).joins(:subject).group(:subject).count(:numerical_response)
     @counts_hash = Hash.new
@@ -33,60 +25,6 @@ class FeedbackInputsController < ApplicationController
     end
   end
 
-  # GET /feedback_inputs/1
-  # GET /feedback_inputs/1.json
-  def show
-  end
-
-  # GET /feedback_inputs/new
-  def new
-    @feedback_input = FeedbackInput.new
-  end
-
-  # GET /feedback_inputs/1/edit
-  def edit
-  end
-
-  # POST /feedback_inputs
-  # POST /feedback_inputs.json
-  def create
-    @feedback_input = FeedbackInput.new(feedback_input_params)
-
-    respond_to do |format|
-      if @feedback_input.save
-        format.html { redirect_to @feedback_input, notice: 'Feedback input was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @feedback_input }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @feedback_input.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /feedback_inputs/1
-  # PATCH/PUT /feedback_inputs/1.json
-  def update
-    respond_to do |format|
-      if @feedback_input.update(feedback_input_params)
-        format.html { redirect_to @feedback_input, notice: 'Feedback input was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @feedback_input.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /feedback_inputs/1
-  # DELETE /feedback_inputs/1.json
-  def destroy
-    @feedback_input.destroy
-    respond_to do |format|
-      format.html { redirect_to feedback_inputs_url }
-      format.json { head :no_content }
-    end
-  end
-
   # /messages
   def voice_messages
     if params[:all] == nil
@@ -100,15 +38,4 @@ class FeedbackInputsController < ApplicationController
                                .order("created_at DESC")
     end
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_feedback_input
-      @feedback_input = FeedbackInput.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def feedback_input_params
-      params.require(:feedback_input).permit(:question_id, :subject_id, :neighborhood_id, :property_id, :voice_file_url, :numberical_response)
-    end
 end
