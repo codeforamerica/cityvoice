e04877803df3850667bc6d3bec09ceea5138d99d
Doc Ritezel <pair+doc@ministryofvelocity.com>
2014-03-07 13:32:31 -0800

    Remove unused helpers

--- app/helpers/notification_subscriptions_helper.rb
+++
@@ -1,2 +0,0 @@
-module NotificationSubscriptionsHelper
-end
--- app/helpers/notifications_mailer_helper.rb
+++ app/helpers/notifications_mailer_helper.rb
@@ -1,9 +1,4 @@
 module NotificationsMailerHelper
-
-  def foo
-    "<h1>FOOOO!!!!</h1>".html_safe
-  end
-
   def feedback_input_type(klass_feedback_input)
     if !klass_feedback_input.voice_file_url.nil?
       return :voice
@@ -11,5 +6,4 @@ module NotificationsMailerHelper
       return :non_voice
     end
   end
-
 end
