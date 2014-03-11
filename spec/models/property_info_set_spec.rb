4e2bed3cc3a5ed6daada3eb4880204bbaebac326
Doc Ritezel <pair+doc@ministryofvelocity.com>
2014-03-11 10:27:06 -0700

    Remove spec for property info set

--- spec/models/property_info_set_spec.rb
+++ spec/models/property_info_set_spec.rb
@@ -1,9 +1,4 @@
 require 'spec_helper'
 
 describe PropertyInfoSet do
-  it "can be created with a property" do
-    @p = Property.create(:name => "456 Test St")
-    @p.property_info_set = PropertyInfoSet.create(:condition_code => 5, :condition => "Poor")
-    @p.property_info_set.condition_code.should eq(5)
-  end
 end
