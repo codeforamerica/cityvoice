d05eb854625f8d7b980853312eb31059f438d71f
Doc Ritezel <pair+doc@ministryofvelocity.com>
2014-03-11 10:17:26 -0700

    Add factories * notification subscription, feedback input, question, voice file and caller

--- spec/factories.rb
+++ spec/factories.rb
@@ -15,4 +15,40 @@ FactoryGirl.define do
     outcome "Vacant and Abandoned"
     demo_order "Affirmed"
   end
+
+  factory :notification_subscription do
+    email { Faker::Internet.email }
+    property
+
+    trait :confirmed do
+      confirmed true
+    end
+
+    trait :bulk_added do
+      bulk_added true
+    end
+  end
+
+  factory :feedback_input do
+    question
+
+    trait :with_voice_file do
+      voice_file_url { Faker::Internet.http_url }
+    end
+  end
+
+  factory :question do
+    voice_text { Faker::Company.bs }
+    short_name { Faker::Name.first_name }
+    feedback_type { %w(numerical_response voice_file).sample }
+  end
+
+  factory :voice_file do
+    short_name { Faker::Name.first_name }
+    url { Faker::Internet.http_url }
+  end
+
+  factory :caller do
+    phone_number { Faker::PhoneNumber.phone_number }
+  end
 end
