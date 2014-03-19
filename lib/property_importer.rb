7ab5bc7be0e6eb536a3ddac3f06773be7f4f0343
Doc Ritezel <pair+doc@ministryofvelocity.com>
2014-03-19 15:56:29 -0700

    Add property importer

---
+++ lib/property_importer.rb
@@ -0,0 +1,44 @@
+require 'csv'
+
+class PropertyImporter < Struct.new(:content)
+  REQUIRED_HEADERS = %w(name property_code lat long).map(&:to_sym)
+
+  def has_valid_headers?
+    missing_headers.empty?
+  end
+
+  def valid?
+    has_valid_headers? && properties.all?(&:valid?)
+  end
+
+  def errors
+    header_errors +
+    properties.reject(&:valid?).map(&:errors).map(&:full_messages).flatten
+  end
+
+  def import
+    properties.each(&:save!) if valid?
+  end
+
+  protected
+
+  def header_errors
+    missing_headers.map { |header| "#{header.to_s.humanize} column is missing" }
+  end
+
+  def missing_headers
+    REQUIRED_HEADERS - headers
+  end
+
+  def csv_entries
+    @csv_entries ||= CSV.new(content, headers: true, header_converters: :symbol).entries
+  end
+
+  def properties
+    csv_entries.map(&:to_hash).map { |p| Property.new(p) }
+  end
+
+  def headers
+    @headers ||= csv_entries.first.headers
+  end
+end
