d435e4250d393ab0caab30fc1c7e1a1a17537c86
CJ Bryan and Doc Ritezel <pair+cj+doc@ministryofvelocity.com>
2014-03-20 16:22:26 -0700

    Remove redundant @content assignment from LandingController

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,6 +1,5 @@
 class LandingController < ApplicationController
   def location_search
-    @content = AppContentSet.first
     @most_recent_messages = FeedbackInput.includes(:subject).where.not(:voice_file_url => nil).order("created_at DESC").limit(3)
   end
 end
