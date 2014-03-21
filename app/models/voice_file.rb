e13287a73c18c9b46bfb0e264f6437af4572aa77
CJ Bryan and Doc Ritezel <pair+cj+doc@ministryofvelocity.com>
2014-03-20 18:07:39 -0700

    Add validations to VoiceFile

--- app/models/voice_file.rb
+++ app/models/voice_file.rb
@@ -1,4 +1,8 @@
 class VoiceFile < ActiveRecord::Base
   has_many :questions
-  attr_accessible :url, :short_name
+
+  attr_accessible :short_name, :url
+
+  validates_presence_of :short_name, :url
+  validates_uniqueness_of :short_name
 end
--- spec/models/voice_file_spec.rb
+++ spec/models/voice_file_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 describe VoiceFile do
   it { should have_many :questions }
+
+  it { should validate_presence_of :short_name }
+  it { should validate_presence_of :url }
+
+  it { should validate_uniqueness_of :short_name }
+
   it { should allow_mass_assignment_of(:url) }
   it { should allow_mass_assignment_of(:short_name) }
 end
