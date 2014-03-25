e3e989179f8869d7c5f529fea5a1d418980f6b02
Cristi Scheye and Doc Ritezel <pair+cristi+doc@ministryofvelocity.com>
2014-03-25 13:47:08 -0700

    extracting FeedbackInput#numerical_response count scopes

--- app/models/feedback_input.rb
+++ app/models/feedback_input.rb
@@ -4,4 +4,12 @@ class FeedbackInput < ActiveRecord::Base
 
   belongs_to :question
   belongs_to :property
+
+  def self.total_calls
+    where.not(numerical_response: nil).joins(:subject).group(:subject).count(:numerical_response)
+  end
+
+  def self.total_responses(numerical_response)
+    where(numerical_response: numerical_response).joins(:subject).group(:subject).count(:numerical_response)
+  end
 end
--- spec/models/feedback_input_spec.rb
+++ spec/models/feedback_input_spec.rb
@@ -4,4 +4,20 @@ describe FeedbackInput do
   it { should belong_to(:subject) }
   it { should belong_to(:property) }
   it { should belong_to(:question) }
+
+  describe '.total_calls' do
+    let!(:input) { create(:feedback_input, :with_property, numerical_response: '1') }
+
+    it 'returns the total number of calls for each property' do
+      expect(FeedbackInput.total_calls).to eq(input.property => 1)
+    end
+  end
+
+  describe '.total_responses' do
+    let!(:input) { create(:feedback_input, :with_property, numerical_response: '1') }
+
+    it 'returns the number of responses for each property' do
+      expect(FeedbackInput.total_responses('1')).to eq(input.property => 1)
+    end
+  end
 end
