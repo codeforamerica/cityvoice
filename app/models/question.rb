c60c883a7cfd0922b7b34917a3f29dd90d0d919a
Doc Ritezel <pair+doc@ministryofvelocity.com>
2014-03-26 15:35:37 -0700

    adding Question.numerical and attribute specs

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,12 @@
 class Question < ActiveRecord::Base
   belongs_to :voice_file
+
   validates_presence_of :short_name, :feedback_type
   validates_uniqueness_of :short_name
+
   attr_accessible :short_name, :feedback_type, :question_text
+
+  def self.numerical
+    where(feedback_type: 'numerical_response')
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -11,4 +11,12 @@ describe Question do
   it { should allow_mass_assignment_of :short_name }
   it { should allow_mass_assignment_of :feedback_type }
   it { should allow_mass_assignment_of :question_text }
+
+  describe '.numerical' do
+    let!(:question) { create(:question, :number) }
+
+    it 'returns all questions with numerical feedback type' do
+      expect(Question.numerical).to eq([question])
+    end
+  end
 end
