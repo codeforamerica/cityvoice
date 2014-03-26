22a15d2afeae1f0d2a8ea1dc877dee31c5e3c80c
Cristi Scheye and Doc Ritezel <pair+cristi+doc@ministryofvelocity.com>
2014-03-26 11:47:04 -0700

    refactoring Subject.find_by_param out of SubjectsController

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,6 +2,14 @@ class Subject < ActiveRecord::Base
   attr_protected
   validates :name, presence: true
 
+  def self.find_by_param(param)
+    if param =~ /^[0-9]+$/
+      find(param)
+    else
+      find_by!(name: param.gsub('-', ' '))
+    end
+  end
+
   def property_code
     digits = content_set.call_in_code_digits.to_i
     self.id.to_s.rjust(digits, '0')
--- spec/models/subject_spec.rb
+++ spec/models/subject_spec.rb
@@ -9,4 +9,28 @@ describe Subject do
       expect(subject.property_code).to eq(subject.id.to_s.rjust(3, '0'))
     end
   end
+
+  describe '.find_by_param' do
+    let!(:property) { create(:property, name: '123 Maple Street') }
+
+    context 'when the subject exists' do
+      it 'finds the subject by id' do
+        expect(Subject.find_by_param(property.id.to_s)).to eq(property)
+      end
+
+      it 'finds the subject by name with dashes' do
+        expect(Subject.find_by_param('123-Maple-Street')).to eq(property)
+      end
+    end
+
+    context 'when the subject does not exist' do
+      it 'blows up when finding by id' do
+        expect { Subject.find_by_param('0') }.to raise_error
+      end
+
+      it 'blows up when finding by name' do
+        expect { Subject.find_by_param('Taco-Trucks') }.to raise_error
+      end
+    end
+  end
 end
