03490b07327d36c8dc85eddf5d71756f56d4f0cb
Doc Ritezel <pair+doc@ministryofvelocity.com>
2014-03-27 22:52:27 -0700

    Remove TextReply

--- spec/support/xpath_matcher.rb
+++
@@ -1,7 +0,0 @@
-RSpec::Matchers.define :match_xpath do |expected|
-  match do |actual|
-    doc = Nokogiri::XML::Document.parse(actual)
-    nodes = doc.xpath(expected)
-    !nodes.empty?
-  end
-end
