60bc691df689fd6c050979ea4c684f31032b85fe
Doc Ritezel <pair+doc@practicalrailsbook.com>
2014-04-08 09:19:47 -0700

    Remove map; refactor answer to no longer create answers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,8 @@
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require leaflet
 //= require backbone-rails
 //= require cityvoice/application
+
+$(function(){ $(document).foundation(); });
