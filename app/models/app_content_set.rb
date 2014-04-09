058cca596cdf90aa46d0bfd9a70d415b6c5d389d
CJ Bryan and Doc Ritezel <pair+cj+doc@ministryofvelocity.com>
2014-04-09 14:59:38 -0700

    Add forwardable accessors to the content hash instead of defining methods.

--- app/models/app_content_set.rb
+++ app/models/app_content_set.rb
@@ -1,39 +1,21 @@
 class AppContentSet < Struct.new(:content_hash)
+  extend Forwardable
+
   def self.load!(path)
     content_hash = AppContentSetImporter.load!(path)
     new(content_hash)
   end
 
-  def feedback_form_url
-    content_hash[:feedback_form_url]
-  end
-
-  def header_color
-    content_hash[:header_color]
-  end
-
-  def message_from
-    content_hash[:message_from]
-  end
-
-  def message_url
-    content_hash[:message_url]
-  end
-
-  def issue
-    content_hash[:issue]
-  end
-
-  def short_title
-    content_hash[:short_title]
-  end
+  def_delegators :content, :app_phone_number,
+                           :feedback_form_url,
+                           :header_color,
+                           :issue,
+                           :message_from,
+                           :message_url,
+                           :short_title
 
   def call_in_code_digits
     content_hash[:call_in_code_digits].to_i
-  end
-
-  def app_phone_number
-    content_hash[:app_phone_number]
   end
 
   def call_text
@@ -49,6 +31,10 @@ class AppContentSet < Struct.new(:content_hash)
   end
 
   protected
+
+  def content
+    @content ||= OpenStruct.new(content_hash)
+  end
 
   def call_renderer
     @call_renderer ||= AppContentRenderer.renderer_for(:call)
