10d9ad7d97dc7a688b7e87cb74dad27d21ac31b2
Dave Guarino <dave@codeforamerica.org>
2015-09-16 16:10:26 -0700

    Fix the activity page to show aggregate responses of each question

--- app/controllers/numerical_answers_controller.rb
+++ app/controllers/numerical_answers_controller.rb
@@ -4,6 +4,7 @@ class NumericalAnswersController < ApplicationController
     agree_counts = Answer.total_responses(1)
     disagree_counts = Answer.total_responses(2)
     counts = AnswerCounts.new(total_counts, agree_counts, disagree_counts)
+    @questions = Question.numerical
     @counts_hash = counts.to_hash
     @sorted_array = counts.to_array
     respond_to do |format|
