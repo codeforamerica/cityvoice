29a1f14be71514e3594bc82955c788588d669551
Dave Guarino <dave@codeforamerica.org>
2015-09-21 12:04:51 -0700

    Load all audio files on all messages page (pagination had been broken), closes #228

--- app/controllers/voice_answers_controller.rb
+++ app/controllers/voice_answers_controller.rb
@@ -1,9 +1,5 @@
 class VoiceAnswersController < ApplicationController
   def index
     @messages = Answer.voice_messages
-
-    if params[:all].nil?
-      @messages = @messages.paginate(page: params[:page], per_page: 10)
-    end
   end
 end
